8dfdc5b8daed2e5680e9e03a360a4a4e0d449790
Godwin Reuben <gutsy258@gmail.com>
2024-03-18 14:29:04 +0100

    adding expense tracker to the git

---
+++ src/components/Transaction.js
@@ -0,0 +1,18 @@
+import React, {useContext} from "react";
+import { GlobalContext } from "../context/GlobalState";
+
+
+export const Transaction = ({transaction}) => {
+  const {deleteTRANSACTION} = useContext(GlobalContext)
+    const sign= transaction.amount< 0 ? '-' : '+';
+  return (
+    <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
+      {transaction.text}{" "}
+      <span>
+        {sign}${Math.abs(transaction.amount)}
+      </span>{" "}
+      <button className="delete-btn">X</button>
+      onClick={()=>deleteTRANSACTION(transaction.id)}
+    </li>
+  );
+};
